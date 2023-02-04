61376a8c6f1c6280e1eb0b243beee51979c02665
Sepehr Aghapour <dev.sepehr@outlook.com>
2023-02-04 18:24:46 +0330

    Project base created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import "./fonts.css";
+function App() {
+  return (
+    <div className="app">
+      <div className="bg-image"></div>
+    </div>
+  );
+}
+
+export default App;
